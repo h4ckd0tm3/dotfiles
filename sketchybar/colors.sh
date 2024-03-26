49153fe51936b68e6b29518852ea957b07ca4a25
h4ckd0tm3 <marcel@schnideritsch.at>
2024-03-26 10:45:29 +0100

    add .config

---
+++ sketchybar/colors.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Color Palette
+#export BLACK=0xff181926
+#export WHITE=0xffcad3f5
+#export RED=0xffed8796
+#export GREEN=0xffa6da95
+#export BLUE=0xff8aadf4
+#export YELLOW=0xffeed49f
+#export ORANGE=0xfff5a97f
+#export MAGENTA=0xffc6a0f6
+#export GREY=0xff939ab7
+#export TRANSPARENT=0x00000000
+
+export BLACK=0xff1F2229
+export WHITE=0xffE6E6E6
+export RED=0xffBF2E5D
+export GREEN=0xff5EBDAB
+export BLUE=0xff367BF0
+export YELLOW=0xffFEA44C
+export ORANGE=0xfff5a97f
+export MAGENTA=0xffBF2E5D
+export GREY=0xff939ab7
+export TRANSPARENT=0x00000000
+
+# General bar colors
+export BAR_COLOR=0xdd1f2229
+export BAR_BORDER_COLOR=0x778C42AB #0xa024273a
+export ICON_COLOR=$WHITE # Color of all icons
+export LABEL_COLOR=$WHITE # Color of all labels
+export BACKGROUND_1=0x603c3e4f
+export BACKGROUND_2=0x60494d64
+
+export POPUP_BACKGROUND_COLOR=0xdd1f2229
+export POPUP_BORDER_COLOR=0x778C42AB
+export POPUP_BACKGROUND_COLOR_SOLID=0xff1f2229
+
+export SHADOW_COLOR=$BLACK
